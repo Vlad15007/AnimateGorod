1927a8eb0f58b952032b6a5360913453cb52bad4
Antonina <polonchuk.a@gmail.com>
2022-07-31 14:57:33 +0300

    <Земля шевелится, домик растет>

--- JavaScript1.js
+++ JavaScript1.js
@@ -1,2 +1,37 @@
 ﻿var $drag = $('.move').draggabilly({})
 
+let house;
+
+$(document).ready(function () {
+    $("#Дом_2").hide();
+    $("#Дом_3").hide();
+    $("#Дом_4").hide();
+    $("#Дом_5").hide();
+    house = 1;
+});
+
+
+    $("#Дом_1").click(function () {
+        $("#Дом_1").hide();
+        $("#Дом_2").show();
+
+    });
+
+$("#Дом_2").click(function () {
+    $("#Дом_2").hide();
+    $("#Дом_3").show();
+
+});
+
+$("#Дом_3").click(function () {
+    $("#Дом_3").hide();
+    $("#Дом_4").show();
+
+});
+
+$("#Дом_4").click(function () {
+    $("#Дом_4").hide();
+    $("#Дом_5").show();
+
+});
+
